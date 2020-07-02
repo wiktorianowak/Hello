242355f13d1eb47fc1d55cdf0191a7e5a570ed51
MegaTron <MegaTron@KOMPUTER>
2020-07-02 11:23:34 +0200

    lesson 7

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        List<float> ratings;
+
+
+        public void AddNewRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float MaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float MinRating()
+        {
+            return ratings.Min();
+        }
+
+        public float CalcAvarageRating()
+        {
+
+            //float avg;
+            //avg = ratings.Average();
+            //return avg;
+
+            float sum = 0, avg = 0;
+
+            if (ratings.Count > 0)
+            {
+                foreach (var rating in ratings)
+                {
+                    sum += rating;
+                }
+                avg = sum / ratings.Count;
+            }
+
+            return avg;
+            
+        }
+
+
+    }
+}
